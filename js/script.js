5d124beac523a38c540414b1a03e78032ba97ab6
CCotuna <calin.cotuna@emanuel.ro>
2023-04-17 01:12:06 +0300

    Designed-Created-Implemented Footer - Made the HOMEPAGE responsive on any display

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+// Responsive menu
+var menuList = document.getElementById("menuList");
 
+menuList.style.maxHeight = "0px";
+function togglemenu(){
+    if( menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "130px"
+    }
+    else{
+        menuList.style.maxHeight = "0px"
+    }
+}
