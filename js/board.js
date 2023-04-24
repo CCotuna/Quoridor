68defb6a11432043c62eb0c0cead65f00fd95424
CCotuna <calin.cotuna@emanuel.ro>
2023-04-24 20:59:55 +0300

    Modified the size of the canvas to fit the board + set a random color for background and for each box

--- js/board.js
+++ js/board.js
@@ -1,15 +1,15 @@
 let board = [];
 
 function setup(){
-    createCanvas(500, 400);
-
-    let color1 = 'green';
+    createCanvas(630, 630);
+    background(color(random(255), random(255), random(255)));
+    let color1 = color(random(255), random(255), random(255));
 
     for(let i = 0; i < 9; i++){
         for(let j = 0; j < 9; j++){
             let x = j * 60 + 50;
             let y = i * 60 + 50;
-            let box = new Box(x, y, 50, green); 
+            let box = new Box(x, y, 50, color1); 
             board.push(box);
         }
     }
