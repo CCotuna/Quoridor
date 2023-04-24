f7ed60478f687c8245778dd38c81408ec7d8a8e4
CCotuna <calin.cotuna@emanuel.ro>
2023-04-24 20:43:25 +0300

    Refreshing the script with innovative revamp

--- js/board.js
+++ js/board.js
@@ -1,38 +1,59 @@
 var canvas;
 
 function setup(){
-    canvas = createCanvas(440, 440)
-    canvas.parent("myCanvas");
+    createCanvas(440, 440)
     background(200)
 }
 
-function draw(){
-    var rows = 0;
-
-    var firstColor = 'black'
-    var secondColor = 'white'
 
 
-    while(rows < 8){
-       if(rows % 2 == 0){
-        firstColor = 'white'
-        secondColor = 'black'
-       }
-       else {
-        firstColor = 'black'
-        secondColor = 'white'
-       }
-        var cols = 0;
-        while(cols <  8){
-            if(cols % 2 == 0){
-                fill(firstColor);
-            }
-            else{
-                fill(secondColor);
-            }
-            rect(cols * 50, rows*50, 50, 50);
-            cols += 1;
-        }
-        rows += 1;
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function draw(){
+//     var rows = 0;
+
+//     var firstColor = 'black'
+//     var secondColor = 'white'
+
+
+//     while(rows < 8){
+//        if(rows % 2 == 0){
+//         firstColor = 'white'
+//         secondColor = 'black'
+//        }
+//        else {
+//         firstColor = 'black'
+//         secondColor = 'white'
+//        }
+//         var cols = 0;
+//         while(cols <  8){
+//             if(cols % 2 == 0){
+//                 fill(firstColor);
+//             }
+//             else{
+//                 fill(secondColor);
+//             }
+//             rect(cols * 50, rows*50, 50, 50);
+//             cols += 1;
+//         }
+//         rows += 1;
+//     }
+// }
