ef6d786a3db19d3f1b8bdc53c70cae4f1c076e4f
CCotuna <calin.cotuna@emanuel.ro>
2023-05-30 13:13:21 +0300

    Created Wall class

--- js/board.js
+++ js/board.js
@@ -41,6 +41,21 @@ class Box {
     }
 }
 
+class Wall {
+    constructor(x, y, w, h, color){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+    }
+
+    display(){
+        fill(this.color);
+        rect(this.x, this.y, this.w, this.h)
+    }
+}
+
 
 function draw(){
     for(let box of board){
