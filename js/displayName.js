abe7754e3d803c3418150735d8adf60ee939999f
CCotuna <calin.cotuna@emanuel.ro>
2023-06-04 20:09:56 +0300

    Switched PVP and PVC divs + changed displayName function so it returns 1 nameInput for PVC or 2 input values for PVP

--- js/displayName.js
+++ js/displayName.js
@@ -1,8 +1,21 @@
-document.getElementById("submitButton").addEventListener("click", function(){
-    let playerName = document.getElementById("playerNameInput").value;
-    localStorage.setItem("playerName", playerName);
-    window.location.href = "loading_page.html";
-})
+
+if(document.getElementById("submitButton1")){
+    document.getElementById("submitButton1").addEventListener("click", function(){
+        let playerName = document.getElementById("playerNameInput").value;
+        localStorage.setItem("playerName", playerName);
+        window.location.href = "loading_page.html";
+    })
+}else{
+    document.getElementById("submitButton2").addEventListener("click", function(){
+        let player1Name = document.getElementById("player1NameInput").value;
+        localStorage.setItem("player1Name", player1Name);
+    
+        let player2Name = document.getElementById("player2NameInput").value;
+        localStorage.setItem("player2Name", player2Name);
+    
+        window.location.href = "loading_page.html";
+    })
+}
 
 function displayPlayerName(playerName){
     textAlign(CENTER, CENTER);
